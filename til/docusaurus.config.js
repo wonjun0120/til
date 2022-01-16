72a6f5a23a835255fe4fb81fc947dd2437aec1f5
wonjun0120 <wonjundero@gmail.com>
2022-01-16 20:03:55 +0900

    Add ga

--- til/docusaurus.config.js
+++ til/docusaurus.config.js
@@ -35,6 +35,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-KNXFDH361W',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
